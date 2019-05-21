f722fccfa36e3c5c2cc11f2a6dad3ef5c003528e
kirillSinkevich <kirill@uniquite.com>
2019-05-21 18:40:17 +0300

    completed header and add main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.svg';
+import iconMenu from './images/icon-menu.png';
+import iconPlane from './images/icon-plane.png';
 import './App.css';
+import Link from './Components/link/link.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header container">
+        <img src={logo} alt="logo"/>
+        <nav className='App-header-navbar'>
+          <Link name='Book & Manage'/>
+          <Link name='Prepare & Travel'/>
+          <Link name='My Account'/>
+        </nav>
       </header>
+      <main className='container'>
+        <div className='left-container'>
+          <div className='main-navbar'>
+            <div class="navbar-icon">
+              <button class="icon-menu" type="image">
+                 <img src={iconMenu} alt="menu"/>
+              </button>
+              <button class="icon-plane" type="image">
+                <img src={iconPlane} alt="plane"/>
+              </button>
+            </div>
+          <nav class="navbar-menu">
+            <ul class="topmenu">
+              <li class="first-li">
+                <a class="topmenu-style">flights</a>
+                <ul class="sub-menu"></ul>
+              </li>
+              <li class="second-li">
+                <a class="topmenu-style">rent car</a>
+                <ul class="rent-car"></ul>
+              </li>
+              <li class="third-li">
+                <a class="topmenu-style">hotel</a>
+                <ul class="hotels"></ul>
+              </li>
+            </ul>
+          </nav>
+          </div>
+        </div>
+        <div className='right-container'>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/Components/link/link.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/*export default class Link extends React.Component {
+	render() {
+		return <a>{this.props.name}</a>;
+	}
+}*/
+//const export  Link = (props) => <a>{props.name}</a>;
+
+export default function Link(props) {
+	return <a>{props.name}</a>;
+}
